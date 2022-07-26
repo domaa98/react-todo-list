28ae857524496e4e013634a33849c2f34cab7d60
domaa98 <modomaa61@gmail.com>
2022-07-26 10:37:29 +0200

    Updates

---
+++ precache-manifest.d1f465d7e78720317890224e02bfd8d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad930a4335994f52b4807a14449266d",
+    "url": "/react-movies-app/index.html"
+  },
+  {
+    "revision": "279ff93c4f1aaf18c344",
+    "url": "/react-movies-app/static/css/main.94021675.chunk.css"
+  },
+  {
+    "revision": "53dbf7356eb4a12d5ad4",
+    "url": "/react-movies-app/static/js/2.3c9e4ee3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movies-app/static/js/2.3c9e4ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279ff93c4f1aaf18c344",
+    "url": "/react-movies-app/static/js/main.3e2634a7.chunk.js"
+  },
+  {
+    "revision": "b53e2530f802d6a48c01",
+    "url": "/react-movies-app/static/js/runtime-main.e5a8e4fa.js"
+  }
+]);
