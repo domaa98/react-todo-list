6ba94b36706d807bfb5aeb129e96506f9162c2cf
domaa98 <modomaa61@gmail.com>
2022-07-26 10:39:02 +0200

    Updates

---
+++ precache-manifest.5567e835f0ee208d1e8ee717de0661d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc6fd5257cb08453de4c666613e52a9",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "279ff93c4f1aaf18c344",
+    "url": "/react-todo-list/static/css/main.94021675.chunk.css"
+  },
+  {
+    "revision": "53dbf7356eb4a12d5ad4",
+    "url": "/react-todo-list/static/js/2.3c9e4ee3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.3c9e4ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279ff93c4f1aaf18c344",
+    "url": "/react-todo-list/static/js/main.3e2634a7.chunk.js"
+  },
+  {
+    "revision": "ccd0048583ea3d63e104",
+    "url": "/react-todo-list/static/js/runtime-main.c7b76107.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-movies-app/precache-manifest.d1f465d7e78720317890224e02bfd8d8.js"
+  "/react-todo-list/precache-manifest.5567e835f0ee208d1e8ee717de0661d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movies-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
